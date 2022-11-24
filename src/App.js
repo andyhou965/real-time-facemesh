86ff958f573942183de0ac86776c6123ef844286
Andy Hou <andyhou965@gmail.com>
2022-11-24 20:07:36 +0400

    draw face mesh

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import "./App.css";
 import { FaceMesh } from "@mediapipe/face_mesh";
-import * as face_mesh from "@mediapipe/face_mesh";
+import * as Facemesh from "@mediapipe/face_mesh";
 import React, { useRef, useEffect } from "react";
 import * as cam from "@mediapipe/camera_utils";
 import Webcam from "react-webcam";
@@ -8,8 +9,62 @@ function App() {
 	const webcamRef = useRef(null);
 	const canvasRef = useRef(null);
 	var camera = null;
+	const connect = window.drawConnectors;
 	function onResults(results) {
-		// console.log(results);
+		// Setting H, W of Canvas
+		const videoWidth = webcamRef.current.video.videoWidth;
+		const videoHeight = webcamRef.current.video.videoHeight;
+
+		// Set canvas width
+		canvasRef.current.width = videoWidth;
+		canvasRef.current.height = videoHeight;
+
+		const canvasElement = canvasRef.current;
+		const canvasCtx = canvasElement.getContext("2d");
+		canvasCtx.save();
+		canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+		canvasCtx.drawImage(
+			results.image,
+			0,
+			0,
+			canvasElement.width,
+			canvasElement.height
+		);
+
+		if (results.multiFaceLandmarks) {
+			for (const landmarks of results.multiFaceLandmarks) {
+				connect(canvasCtx, landmarks, Facemesh.FACEMESH_TESSELATION, {
+					color: "#C0C0C070",
+					lineWidth: 1,
+				});
+				connect(canvasCtx, landmarks, Facemesh.FACEMESH_RIGHT_EYE, {
+					color: "#FF3030",
+				});
+				connect(canvasCtx, landmarks, Facemesh.FACEMESH_RIGHT_EYEBROW, {
+					color: "#FF3030",
+				});
+				connect(canvasCtx, landmarks, Facemesh.FACEMESH_RIGHT_IRIS, {
+					color: "#FF3030",
+				});
+				connect(canvasCtx, landmarks, Facemesh.FACEMESH_LEFT_EYE, {
+					color: "#30FF30",
+				});
+				connect(canvasCtx, landmarks, Facemesh.FACEMESH_LEFT_EYEBROW, {
+					color: "#30FF30",
+				});
+				connect(canvasCtx, landmarks, Facemesh.FACEMESH_LEFT_IRIS, {
+					color: "#30FF30",
+				});
+				connect(canvasCtx, landmarks, Facemesh.FACEMESH_FACE_OVAL, {
+					color: "#E0E0E0",
+				});
+				connect(canvasCtx, landmarks, Facemesh.FACEMESH_LIPS, {
+					color: "#E0E0E0",
+				});
+			}
+		}
+
+		canvasCtx.restore();
 	}
 	// }
 
